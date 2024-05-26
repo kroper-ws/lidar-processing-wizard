3bcaea56b85c013b02b14f5d5feb7a087eab974d
kroper-ws <167791876+kroper-ws@users.noreply.github.com>
2024-05-26 13:30:33 -0600

    Update run_lasinfo_tab.py

--- src/gui/run_lasinfo_tab.py
+++ src/gui/run_lasinfo_tab.py
@@ -3,8 +3,7 @@ from PyQt5.QtWidgets import QCheckBox, QSpinBox, QLineEdit, QVBoxLayout
 from gui.base_tab import BaseTab
 from utils.form_builder import FormBuilder
 from processing.run_lasinfo import run_lasinfo
-from processing.params_config import GENERAL_PARAMS, BASIC_PARAMS, LASINFO_PARAMS, ADDITIONAL_GENERAL_PARAMS
-
+from processing.params_config import get_param_group, load_params
 
 class LasInfoTab(BaseTab):
     """
@@ -19,6 +18,8 @@ class LasInfoTab(BaseTab):
             emitter (logging.Logger): The log emitter for logging messages.
         """
         self.default_directory = default_directory
+        params = load_params()
+        self.LASINFO_PARAMS = get_param_group(params, "LASINFO_PARAMS")
         super().__init__('View LAS Info', self.run_process, emitter)
 
     def initUI(self):
@@ -43,28 +44,7 @@ class LasInfoTab(BaseTab):
         Args:
             form_builder (FormBuilder): The form builder instance.
         """
-        sections = [
-            ("General Parameters", GENERAL_PARAMS),
-            ("Basic Parameters", BASIC_PARAMS),
-            ("Additional General Parameters", ADDITIONAL_GENERAL_PARAMS),
-            ("LASInfo Parameters", LASINFO_PARAMS),
-        ]
-
-        for section_name, params in sections:
-            self.add_parameters(form_builder, section_name, params)
-
-    def add_parameters(self, form_builder: FormBuilder, section_name: str, params: dict):
-        """
-        Add parameters to the form builder under a specific section.
-
-        Args:
-            form_builder (FormBuilder): The form builder instance.
-            section_name (str): The name of the section.
-            params (dict): The parameters to add.
-        """
-        form_builder.add_collapsible_section(section_name, params)
-
-        for param, config in params.items():
+        for param, config in self.LASINFO_PARAMS.items():
             if config['type'] == 'checkbox':
                 self.params_widgets[param] = form_builder.add_checkbox(config['label'], config.get('default', False))
             elif config['type'] == 'spinbox':
@@ -89,10 +69,7 @@ class LasInfoTab(BaseTab):
             elif isinstance(widget, QLineEdit):
                 text = widget.text()
                 if text:
-                    if param in ['set_bb', 'set_bounding_box', 'set_creation_date', 'set_number_of_points_by_return']:
-                        params[param] = text.split()
-                    else:
-                        params[param] = text
+                    params[param] = text
         return params
 
     def run_process(self, input_path: str, **params):
